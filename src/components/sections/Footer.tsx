424e5490337e2e3ac7a5ad8238516146c9af055e
csvidit <63170797+csvidit@users.noreply.github.com>
2024-07-13 22:10:18 -0400

    edit footer

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -2,11 +2,25 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <span className="inline-flex shrink-0 flex-row flex-wrap whitespace-pre-wrap items-center gap-1 text-xs font-light text-brat-800 *:shrink-0">
-      <p>
-        A Snippetopia template made by Vidit Khandelwal. Check out more on{" "}
-        <Link href="https://snippetopia.xyz">snippetopia.xyz</Link>
-      </p>
+    <span className="inline-flex shrink-0 flex-row flex-wrap items-center gap-1 whitespace-pre-wrap text-xs font-light text-brat-800 *:shrink-0">
+      <span>
+        A Snippetopia template made by{" "}
+        <Link
+          className="inline whitespace-pre-wrap"
+          href="https://viditkhandelwal.com"
+        >
+          Vidit Khandelwal
+        </Link>
+      </span>
+      <span>
+        Check out more on{" "}
+        <Link
+          className="inline whitespace-pre-wrap"
+          href="https://snippetopia.xyz"
+        >
+          snippetopia.xyz
+        </Link>
+      </span>
     </span>
   );
 };
