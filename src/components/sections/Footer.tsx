8281069676e7e61c45b842ee303023d7e2bb2f1a
csvidit <63170797+csvidit@users.noreply.github.com>
2024-07-13 22:06:09 -0400

    fix mobile overflow

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <span className="inline-flex shrink-0 flex-row flex-wrap items-center gap-1 text-xs font-light text-brat-800 *:shrink-0">
-      <span>
+    <span className="inline-flex shrink-0 flex-row flex-wrap whitespace-pre-wrap items-center gap-1 text-xs font-light text-brat-800 *:shrink-0">
+      <p>
         A Snippetopia template made by Vidit Khandelwal. Check out more on{" "}
         <Link href="https://snippetopia.xyz">snippetopia.xyz</Link>
-      </span>
+      </p>
     </span>
   );
 };
